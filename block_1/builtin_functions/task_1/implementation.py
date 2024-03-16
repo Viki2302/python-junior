e69cdb5f1393673e5cf6e688808469f46fa5f13d
v.konevskikh <v.konevskikh@bars-open.ru>
2024-03-16 19:48:43 +0300

    decorators(task_1) and builtin_functions

--- block_1/builtin_functions/task_1/implementation.py
+++ block_1/builtin_functions/task_1/implementation.py
@@ -1,2 +1,22 @@
+
 class Value:
-    pass
+    def __init__(self, val):
+        self.val = val
+        
+    def get_value(self):
+        return self.val
+        
+    def __add__(self, other):
+        return self.val + other
+        
+    def __sub__(self, other):
+        return self.val - other
+        
+    def __mul__(self, other):
+        return self.val * other
+        
+    def __truediv__(self, other):
+        if other != 0:
+            return self.val / other
+        else:
+            raise ValueError("Division by zero")
--- block_1/decorators/task_1/implementation.py
+++ block_1/decorators/task_1/implementation.py
@@ -1,6 +1,23 @@
-def time_execution():
-    """
-    Обертка, печатающая время выполнения функции.
-    """
+def time_execution(function):
+    def wrapper(n):
+        import time
+ 
+        # начальное время
+        start_time = time.time()
+        function(n)
+        # конечное время
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(elapsed_time)
+ 
+    return wrapper
+    
+def factorial(n):
+    count = 0
+    while n >=5:
+        n //= 5
+        count += n
+    return count
 
-    raise NotImplementedError
+decorate = time_execution(factorial)
+decorate(5)
